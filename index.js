ea86f2ff05a15c8e2c6c979aed35a124e199aec3
Raymond <raymond.mwaiki@student.moringaschool.com>
2023-10-06 11:57:35 +0300

    Done

--- index.js
+++ index.js
@@ -1,2 +1,10 @@
 
 // Write your code here
+let num1=31, num2 =2
+let multiply=num1*num2;
+
+let random= Math.floor(Math.random()*10)+1
+let max=Math.max(2,4,6,8,9,20,3,7);
+
+let num3=24, num4 =5;
+let mod=num3%num4;
